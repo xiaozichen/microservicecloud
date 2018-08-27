8e8da5e750352bbbdb6d6a383d0a0785bb91cd1e
shanchen3 <11473446@qq.com>
2018-08-27 10:28:26 +0800

    提交分支dev

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -20,10 +20,10 @@ public class Dept implements Serializable// entity --orm--- db_table
 	private String 	dname; // 部门名称
 	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	
-	public Dept(String dname)
+	public Dept(String dname3)
 	{
 		super();
-		this.dname = dname;
+		this.dname = dname3;
 	}
 	
 	
